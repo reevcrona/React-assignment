c58608ce8293b7cbfc1e463c018da6a035bc6f89
Jreev <jacob.reevcrona@yahoo.com>
2024-11-20 12:19:18 +0100

    added home page and albums page with some content

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,25 @@
 import { useState } from 'react'
-import AlbumCard from './AlbumCard'
+import Home from "./Pages/Home.jsx";
+import Albums from "./Pages/Albums.jsx";
 import "./App.css";
-import ThrillerImg from "./assets/thriller-album.jpg";
-import BackInBlackImg from "./assets/BackInBlack.svg"
-import TheBodyGuardImg from "./assets/theBodyGuard.jpg";
-import TheDarkSideOftheMoonImg from "./assets/TheDarkSideoftheMoon.webp";
-import BatOutOfHellImg from "./assets/Bat Out of Hell.jpg";
 
-const albums = [
-  {
-    albumTitle:"Thriller",
-    albumCover:ThrillerImg,
-    Artist:"Michael Jackson",
-    price:18.69
-  },
-  {
-    albumTitle:"Back In Black",
-    albumCover:BackInBlackImg,
-    Artist:"AC/DC",
-    price:26.98
-  },
-  {
-    albumTitle:"The Bodyguard",
-    albumCover:TheBodyGuardImg,
-    Artist:"Whitney Houston",
-    price:19.30
-  },
-  {
-    albumTitle:"The Dark Side of the Moon",
-    albumCover:TheDarkSideOftheMoonImg,
-    Artist:"Pink Floyd",
-    price:52.99
-  },
-  {
-    albumTitle:"Bat Out of Hell",
-    albumCover:BatOutOfHellImg,
-    Artist:"Meatloaf",
-    price:27.91
-  }
-]
+
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
 
 
 function App() {
   
   return (
-    <div className="main-container">
-      {albums.map((album) => {
-        return(
-          <AlbumCard 
-            key = {album.albumTitle}
-            title = {album.albumTitle}
-            cover = {album.albumCover}
-            artist = {album.Artist}
-            albumPrice = {album.price}
-          />
-        )
-      })}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element = {<Home />}/>
+          <Route path="/albums" element = {<Albums />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
