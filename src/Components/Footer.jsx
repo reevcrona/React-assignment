2f98d4eb744b6d7a659ddea6e329817e6800eeeb
Jreev <jacob.reevcrona@yahoo.com>
2024-11-20 15:36:52 +0100

    added some icons and basic styling for footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter,faFacebookF,faInstagram} from '@fortawesome/free-brands-svg-icons';
 
-
+import "../styles/Footer.css";
 function Footer(){
     return (
-        <div>
-            footer
-        </div>
+        <footer>
+            <nav>
+                <ul>
+                    <li>
+                        <a href=""><FontAwesomeIcon className="logo" icon={faXTwitter} /></a>
+                    </li>
+                    <li>
+                        <a href=""><FontAwesomeIcon className="logo" icon={faFacebookF}/></a>
+                    </li>
+                    <li>
+                        <a href=""><FontAwesomeIcon className="logo" icon={faInstagram} /></a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
     )
 }
 
