747d0d45626f69970f34be95bb710c131b3a0bfc
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-11-20 06:53:03 +0100

    added information to the cards

--- src/AlbumCard.jsx
+++ src/AlbumCard.jsx
@@ -3,8 +3,11 @@ import "./AlbumCard.css";
 
 function AlbumCard(props){
     return(
-        <div>
-            <img className="album-cover" src={props.image}></img>
+        <div className="album-card-container">
+            <img className="album-cover" src={props.cover}></img>
+            <h2>{props.title}</h2>
+            <h3>Artist: {props.artist}</h3>
+            <h4>Amazon price: {props.albumPrice}</h4>
         </div>
     )
 }
