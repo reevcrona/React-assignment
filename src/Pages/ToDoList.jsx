d459728bf1091782a431a6683b79bf7fbd2d8ada
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-11-27 14:21:13 +0100

    added logic so user can add items to their list and it renders on the screen

--- src/Pages/ToDoList.jsx
+++ src/Pages/ToDoList.jsx
@@ -1,9 +1,38 @@
-
+import { useState } from "react";
 
 function ToDoList(){
+
+    const [toDoList,setToDoList] = useState([]);
+    const [taskValue,setTaskValue] =  useState("");
+
+
+    const handleInputValue = (e) => {
+        setTaskValue(e.target.value)
+    }
+
+    const addTask = () => {
+        if(taskValue){
+            setToDoList((prevState) => [...prevState,taskValue]);
+            setTaskValue("");
+        }
+    }
+
+    const renderList = () => {
+      return toDoList.map((task,index) => <li key={index}>{task}</li>)
+    }
+    
+    
+    console.log(toDoList)
+    
+
     return(
-        <div>
-            Mista list
+        <div className="to-do-main-container">
+            <ul>
+                {renderList()}
+            </ul>
+
+            <input onChange={handleInputValue} value={taskValue} placeholder="Task"></input>
+            <button onClick={addTask}>Add task</button>
         </div>
     )
 }
